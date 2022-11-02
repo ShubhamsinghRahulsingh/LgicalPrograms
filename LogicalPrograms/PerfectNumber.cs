1d9e2713fbafe29976e52cb303e88e2caf0d5899
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-02 09:35:40 +0530

    (Shubham)Add: checking a given number is perfect or not

---
+++ LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        public void Check(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i <=num/2 ; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            if(num==sum)
+                Console.WriteLine("Number we have is a perfect Number");
+            else
+                Console.WriteLine("Number we have is not a perfect Number");
+        }
+
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
-
 namespace LogicalPrograms
 {
     class Program
     {
         static void Main(string[] args) //Entry point of our application
         {
-            FibonacciSeries print = new FibonacciSeries();
-            print.Series(10);
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.FibonacciSeries" + "\n" + "2.PerfectNumber" + "\n" + "3.Exit");
+                Console.WriteLine("Enter your choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                   case 1:
+                      FibonacciSeries print = new FibonacciSeries();
+                      print.Series(10);
+                   break;
+                   case 2:
+                       PerfectNumber check = new PerfectNumber();
+                        check.Check(28);
+                        break;
+                   case 3:
+                        flag= false;
+                        break;
+            }  
+            }
         }
     }
 }
