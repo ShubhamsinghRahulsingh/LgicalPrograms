a8b6e0fe8fe690c7e69262edee6fa0776de79aff
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-11-02 08:55:49 +0530

    (Shubham)Add: printing Fibonacci series

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args) //Entry point of our application
+        {
+            FibonacciSeries print = new FibonacciSeries();
+            print.Series(10);
+        }
+    }
+}
